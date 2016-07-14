42107f3181c7b36d77f7e59defc40362e6c36c4d
MF1523017 <554896649@qq.com>
2016-07-14 21:44:36 +0800

    the arg is a class for thread

--- thread1.py
+++ thread1.py
@@ -10,18 +10,31 @@ from time import sleep,ctime
 
 loops=[4,2]
 #创建一个Thread的实例，传给它一个函数
+
 def loop(nloop,nsec):
     print 'start loop',nloop,'at:',ctime()
     sleep(nsec)
     print 'loop',nloop,'done at:',ctime()
 #创建一个Thread 的实例，传给它一个类
 
+
+class ThreadFunc(object):
+    def __init__(self,func,args,name=''):
+        self.name=name
+        self.func=func
+        self.args=args
+    def __call__(self):
+        apply(self.func,self.args)
+#    def loop(nloop,nsec):
+#        print 'start loop',nloop,'at:',ctime()
+#        sleep(nsec)
+#        print 'loop',nloop,'done at:',ctime()
 if __name__=='__main__':
     print 'starting at:',ctime()
     threads=[]
     nloops=range(len(loops))
     for i in nloops:
-        t=threading.Thread(target=loop,args=(i,loops[i]))
+        t=threading.Thread(target=ThreadFunc(loop,(i,loops[i]),loop.__name__))
         threads.append(t)
     
     for i in nloops:
