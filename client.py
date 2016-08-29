7697fab7bfe48e80ad74c1059a6ddca8feb37209
MF1523017 <554896649@qq.com>
2016-08-29 16:13:33 +0800

    some little commont

--- client.py
+++ client.py
@@ -9,6 +9,6 @@ Created on Thu Jul 21 16:11:06 2016
 from tcp import TcpClient
 
 if __name__=='__main__':
-    tc=TcpClient('localhost',23456,1024)
+    tc=TcpClient('localhost',23456,1024)#change the ipaddr if you use the code
     tc.build()
     tc.communication()
